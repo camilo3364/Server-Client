136612e6b8ffba607fef6fee3982b6c5fb0c4201
camilo3364 <camijuan_321@hotmail.com>
2022-09-30 16:25:47 -0500

    Try to create a client class for file name

---
+++ src/client/clientClass.go
@@ -0,0 +1,15 @@
+package client
+
+import "fmt"
+
+type Client struct {
+	Fiel string
+}
+
+func EnterName() string {
+	var nameFile string
+	fmt.Println("Enter the name of the file, for example (hello.txt o image.jpg): ")
+	fmt.Scan(&nameFile)
+	return nameFile
+
+}
