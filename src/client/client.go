4824076af03097a4770ef9d36cc80328c276b72f
camilo3364 <camijuan_321@hotmail.com>
2022-09-30 12:45:50 -0500

    client initialized

--- src/client/client.go
+++ src/client/client.go
@@ -1,10 +1,15 @@
 package client
 
-import "fmt"
+import (
+	"bytes"
+	"fmt"
+	"net"
+	"os"
+)
 
 type Client struct {
-	Name       string
-	Credential int
+	Name   string
+	Serial int
 }
 
 func EnterName() string {
@@ -12,5 +17,53 @@ func EnterName() string {
 	fmt.Println("Enter the name of the file, for example (hello.txt o image.jpg): ")
 	fmt.Scan(&nameFile)
 	return nameFile
+
+}
+
+func client(src, dataType string) {
+
+	filerc, err := os.Open(src + dataType)
+	print(src + dataType)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	defer filerc.Close()
+
+	buf := new(bytes.Buffer)
+	buf.ReadFrom(filerc)
+	contents := buf.String()
+
+	fmt.Print(contents)
+	//send file between client and server
+	c, err := net.Dial("tcp", ":9999")
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	c.Write([]byte(contents))
+	//
+	EnterName()
+	//c.Write([]byte(contents))
+	//save the imgByte to file
+	//out, err := os.Create("./QRImg.png")
+
+	c.Close()
+
+}
+
+func main() {
+
+	src := "/Programming/serverProyect/archivos/"
+	dataType := "prueba1.txt"
+	fmt.Println(dataType)
+	//enterName()
+	//go EnterName()
+	go client(src, dataType)
+
+	//f := "/Programming/serverProyect/archivos/prueba1.txt"
+	//openFile(f)
+	var input string
+	fmt.Scanln(&input)
 
 }
