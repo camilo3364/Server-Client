d2958984835f11aa3c29a51ddbd02a2b27f88caf
camilo3364 <camijuan_321@hotmail.com>
2022-10-03 21:50:35 -0500

    The channels are completed

--- src/client/client.go
+++ src/client/client.go
@@ -9,7 +9,45 @@ import (
 	"sync"
 )
 
-func client(src, dataType string, wg *sync.WaitGroup) {
+/*
+	func connectChannel(channel string) {
+		channel1 := "111111111."
+		//channel2 := "222222222."
+		c, err := net.Dial("tcp", ":9999")
+		if err != nil {
+			fmt.Println(err)
+			return
+		}
+		c.Write([]byte(channel1))
+		c.Write([]byte(channel))
+
+		c.Close()
+	}
+*/
+
+func listen() {
+	var counter int
+	s, err := net.Listen("tcp", ":9999")
+
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	for {
+		c, err := s.Accept()
+		if err != nil {
+			fmt.Println(err)
+
+			continue
+		}
+		counter += 1
+		fmt.Println("A Client sent a file")
+		fmt.Println(c)
+		//go handleClient2(c, counter)
+	}
+
+}
+func client(src, dataType string, wg *sync.WaitGroup, channel string) {
 
 	filerc, err := os.Open(src + dataType)
 	if err != nil {
@@ -28,6 +66,7 @@ func client(src, dataType string, wg *sync.WaitGroup) {
 		fmt.Println(err)
 		return
 	}
+	c.Write([]byte(channel))
 	c.Write([]byte(contents))
 
 	defer wg.Done()
@@ -41,10 +80,25 @@ func main() {
 		//variables
 		var wg sync.WaitGroup
 		var name string
+		var channel string
+		var channelOut string
 		src := "/Programming/codigos_go/serverClient/src/fields/"
 
+		//Channel suscribe
+		fmt.Println("You need to join the channel (1 or 2): ")
+		fmt.Scan(&channel)
+		if channel == "1" {
+			channelOut = "1chanel_..."
+		} else if channel == "2" {
+			channelOut = "2chanel_..."
+		} else {
+			fmt.Println("Please, you should select a channel ")
+			main()
+		}
+
+		//go connectChannel(channel)
 		//Message
-		fmt.Println("Please write the name of the file for exampe (image.png or hello.txt): ")
+		fmt.Println("Please write the name of the file for example (image.png or hello.txt): ")
 		fmt.Scan(&name)
 
 		var input string
@@ -53,7 +107,7 @@ func main() {
 		dataType := string(name)
 
 		wg.Add(1)
-		go client(src, dataType, &wg)
+		go client(src, dataType, &wg, channelOut)
 		wg.Wait()
 		fmt.Println("Do you need send other file? Yes or No")
 		fmt.Scan(&name)
--- src/server.go
+++ src/server.go
@@ -7,11 +7,13 @@ import (
 	"net"
 	"os"
 	"strconv"
+	"strings"
 )
 
 func servidor() {
 	var counter int
 	s, err := net.Listen("tcp", ":9999")
+
 	if err != nil {
 		fmt.Println(err)
 		return
@@ -24,22 +26,15 @@ func servidor() {
 			continue
 		}
 		counter += 1
-		fmt.Println("A Client is connected")
-		go handleClient(c, counter)
+		fmt.Println("A Client sent a file")
+		go handleClient2(c, counter)
 	}
 
 }
 
 func handleClient(c net.Conn, counter int) {
 
-	//fmt.Println(c)
-	//var client pk.Client
-	//name := string(pk.EnterName())
-	//client.Fiel = string(pk.EnterName())
-	//client.Fiel = "archivo.txt"
 	datatype1 := "salida" + strconv.Itoa(counter)
-
-	//fmt.Println(client)
 
 	b := make([]byte, 1000000)
 	bs, err := c.Read(b)
@@ -47,11 +42,10 @@ func handleClient(c net.Conn, counter int) {
 		fmt.Println(err)
 		return
 	} else {
-		//datatype := string(b[:bs])
+
 		fmt.Println("Bytes", bs)
 
 		reader := bytes.NewReader(b)
-		//dst := "/Programming/serverProyect/carpeta_salida/" + datatype
 		out, err := os.Create("/Programming/codigos_go/serverClient/src/output/" + datatype1)
 		if err != nil {
 			fmt.Println(err)
@@ -65,10 +59,70 @@ func handleClient(c net.Conn, counter int) {
 	}
 }
 
+func handleClient2(c net.Conn, counter int) {
+
+	ch1 := make(chan string, 1)
+	ch2 := make(chan string, 1)
+	ch1 <- "5555"
+	ch2 <- "8080"
+
+	//datatype1 := "salida" + strconv.Itoa(counter)
+
+	b := make([]byte, 1000000)
+	bs, err := c.Read(b)
+	if err != nil {
+		fmt.Println(err)
+		return
+	} else {
+
+		fmt.Println("Bytes", bs)
+
+		//reader := bytes.NewReader(b)
+		reader2 := string(b[:bs])
+		fmt.Println(reader2)
+		split1 := strings.Split(reader2, "chanel_...")
+		split2 := strings.Split(reader2, "chanel_...")
+
+		if split1[0] == ("1") {
+			//send to message
+			//send file between client and server
+			c, err := net.Dial("tcp", ":8080")
+			if err != nil {
+				fmt.Println(err)
+				return
+			}
+			c.Write([]byte(split1[1]))
+			c.Close()
+			//
+			fmt.Println("Ingresamos al chanel 1")
+			/*out, err := os.Create("/Programming/codigos_go/serverClient/src/output/" + datatype1)
+			if err != nil {
+				fmt.Println(err)
+			}
+			defer out.Close()
+
+			_, err = io.Copy(out, reader)
+			if err != nil {
+				fmt.Println(err)
+			}
+			*/
+
+		} else if split2[0] == ("2") {
+			fmt.Println("Ingresamos al chanel2")
+			c, err := net.Dial("tcp", ":5555")
+			if err != nil {
+				fmt.Println(err)
+				return
+			}
+			c.Write([]byte(split2[1]))
+			c.Close()
+		}
+
+	}
+}
+
 func main() {
 
-	//src := "/Programming/serverProyect/archivos/yolov3.png"
-	//dst := "/Programming/serverProyect/carpeta_salida/output.png"
 	go servidor()
 
 	//Copy(src, dst)
