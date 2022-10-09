ce67aed44f058008f649cc6cd6032a515b4e604f
camilo3364 <camijuan_321@hotmail.com>
2022-10-09 12:55:11 -0500

    Server send the file to client in different ch

--- src/client/client.go
+++ src/client/client.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"bytes"
+	"crypto/rand"
 	"fmt"
 	"net"
 	"os"
@@ -9,44 +10,76 @@ import (
 	"sync"
 )
 
-/*
-	func connectChannel(channel string) {
-		channel1 := "111111111."
-		//channel2 := "222222222."
-		c, err := net.Dial("tcp", ":9999")
-		if err != nil {
-			fmt.Println(err)
-			return
-		}
-		c.Write([]byte(channel1))
-		c.Write([]byte(channel))
-
-		c.Close()
-	}
-*/
-
-func listen() {
-	var counter int
-	s, err := net.Listen("tcp", ":9999")
-
+func sendTcp(tcp string) {
+	name := []byte(tcp)
+	c, err := net.Dial("tcp", ":8080")
 	if err != nil {
 		fmt.Println(err)
 		return
 	}
-	for {
-		c, err := s.Accept()
-		if err != nil {
-			fmt.Println(err)
+	fmt.Println(name)
+	c.Write([]byte(tcp))
 
-			continue
+	c.Close()
+
+}
+func listen(channel string) {
+	var counter int
+	for {
+		if channel == "1" {
+			s, err := net.Listen("tcp", ":8080")
+			if err != nil {
+
+				main()
+				continue
+
+				//fmt.Println(err)
+				//fmt.Println("Ocurrio error en 1")
+				//return
+			}
+			c, err := s.Accept()
+			if err != nil {
+				//main()
+				fmt.Println(err)
+				fmt.Println("Ocurrio error en 2")
+				continue
+			}
+			counter += 1
+			fmt.Println("You receive a file by ch1")
+			fmt.Println(c)
+			//go handleClient2(c, counter)
+			s.Close()
+		} else if channel == "2" {
+			fmt.Println("You receive a file by ch2")
+			r, err := net.Listen("tcp", ":5555")
+			if err != nil {
+				//r.Close()
+				main()
+				continue
+				//fmt.Println(err)
+				//fmt.Println("Ocurrio error en 3")
+
+				//return
+
+			}
+			n, err := r.Accept()
+			if err != nil {
+				fmt.Println(err)
+				fmt.Println("Ocurrio error en 4")
+
+				continue
+			}
+			counter += 1
+			fmt.Println("You receive a file by ch2")
+			fmt.Println(n)
+			//go handleClient2(c, counter)
+			r.Close()
 		}
-		counter += 1
-		fmt.Println("A Client sent a file")
-		fmt.Println(c)
-		//go handleClient2(c, counter)
+
 	}
 
 }
+
 func client(src, dataType string, wg *sync.WaitGroup, channel string) {
 
 	filerc, err := os.Open(src + dataType)
@@ -75,52 +108,83 @@ func client(src, dataType string, wg *sync.WaitGroup, channel string) {
 }
 
 func main() {
-
 	for {
 		//variables
+		RandomCrypto, _ := rand.Prime(rand.Reader, 12)
+		randomNumber := RandomCrypto.String()
+		fmt.Println(randomNumber)
 		var wg sync.WaitGroup
 		var name string
 		var channel string
 		var channelOut string
+		var listen1 string
 		src := "/Programming/codigos_go/serverClient/src/fields/"
 
-		//Channel suscribe
+		fmt.Println("You need to shoose if receive(1) or send (2) a file: ")
+		fmt.Scan(&listen1)
+		//Do you need listen o write the message?
 		fmt.Println("You need to join the channel (1 or 2): ")
 		fmt.Scan(&channel)
 		if channel == "1" {
-			channelOut = "1chanel_..."
+			channelOut = "1chanel_..." + randomNumber + "_..."
+			sendTcp(randomNumber)
+			fmt.Println(randomNumber)
+			fmt.Println(channel)
 		} else if channel == "2" {
-			channelOut = "2chanel_..."
+			channelOut = "2chanel_..." + randomNumber + "_..."
 		} else {
 			fmt.Println("Please, you should select a channel ")
 			main()
 		}
 
-		//go connectChannel(channel)
-		//Message
-		fmt.Println("Please write the name of the file for example (image.png or hello.txt): ")
-		fmt.Scan(&name)
+		if listen1 == "1" {
+			//flag := false
+			//var loop string
+			fmt.Println("In this momento you are listen a message...")
+			go listen(channel)
+			/*for flag == false {
+				fmt.Println("In this momento you are listen a message...")
+				go listen(channel)
+				fmt.Println("Do you want to receive a new file (1) or go out (2)")
+				fmt.Scan(&loop)
+				if loop == "1" {
+					flag = true
+				} else {
+					defer listen(channel)
+				}
 
-		var input string
-		fmt.Scanln(&input)
+			}*/
 
-		dataType := string(name)
+		} else if listen1 == "2" {
 
-		wg.Add(1)
-		go client(src, dataType, &wg, channelOut)
-		wg.Wait()
-		fmt.Println("Do you need send other file? Yes or No")
-		fmt.Scan(&name)
+			//go connectChannel(channel)
+			//Message
+			fmt.Println("Please write the name of the file for example (image.png or hello.txt): ")
+			fmt.Scan(&name)
 
-		if strings.ToLower(string(name)) == "yes" {
-			print("Ingreso al if")
-			main()
-		} else if strings.ToLower(string(name)) == "no" {
-			break
-		} else {
+			var input string
+			fmt.Scanln(&input)
+
+			dataType := string(name)
+
+			wg.Add(1)
+			go client(src, dataType, &wg, channelOut)
+			wg.Wait()
 			fmt.Println("Do you need send other file? Yes or No")
-			continue
+			fmt.Scan(&name)
+
+			if strings.ToLower(string(name)) == "yes" {
+				print("Ingreso al if")
+				main()
+			} else if strings.ToLower(string(name)) == "no" {
+				break
+			} else {
+				fmt.Println("Do you need send other file? Yes or No")
+				continue
+			}
 		}
+
+		//Channel suscribe
 
 	}
 
